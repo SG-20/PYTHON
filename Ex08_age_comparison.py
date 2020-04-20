ed665a5e58583985b4c129f13513fc1e6852a1a6
SG-20 <59667884+SG-20@users.noreply.github.com>
2020-04-20 12:00:52 +1200

    Add files via upload

---
+++ Ex08_age_comparison.py
@@ -0,0 +1,9 @@
+my_age=50
+user_age=int(input("Type your age:"))
+
+if(user_age>my_age):
+              print("Your're older than me")
+elif(user_age==my_age):
+              print("We are the same age")
+else:
+              print("You're younger than me")
